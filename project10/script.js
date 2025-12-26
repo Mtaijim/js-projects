c2f146f480c5107cbde7d232cfb9099c4ada2dd4
Mtaijim shaikh <mhdtaijim07@gmail.com>
2025-12-26 16:12:34 +0530

    project 9

--- project10/script.js
+++
@@ -1,7 +0,0 @@
-const popup = document.getElementById("open-popup");
-function openpopup() {
-  popup.classList.add("openpopup");
-}
-function closepopup() {
-  popup.classList.remove("openpopup");
-}
