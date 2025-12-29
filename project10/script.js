dde8059d31ccc2f0cba1400018a6c1dc215b0e11
Mtaijim shaikh <mhdtaijim07@gmail.com>
2025-12-29 10:55:21 +0530

    project10 : submit button

---
+++ project10/script.js
@@ -0,0 +1,7 @@
+const popup = document.getElementById("open-popup");
+function openpopup() {
+  popup.classList.add("openpopup");
+}
+function closepopup() {
+  popup.classList.remove("openpopup");
+}
